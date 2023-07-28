9875962036036f3a67f0431242d4211c1d40b496
mobai <2813089832@qq.com>
2023-07-28 13:48:32 +0800

    feat: 修改video

--- packages/element/components/video/src/video.vue
+++ packages/element/components/video/src/video.vue
@@ -15,66 +15,20 @@ const props = withDefaults(defineProps<VideoProps>(), {
 
 })
 const attrs = useAttrs()
-const coverSrc = ref<string | undefined>()
-
-/**
- * 获取视频封面
- * @param url 视频地址
- * @param time 事件
- */
-const getVideoCover = (url: string, time: number): Promise<string> => {
-  return new Promise(resolve => {
-    const video = document.createElement('video');
-    video.src = url;
-
-    video.onloadeddata = function() {
-      video.currentTime = time;
-
-      const canvas = document.createElement('canvas');
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-
-      const context = canvas.getContext('2d');
-      context?.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-      const dataURL = canvas.toDataURL('image/png');
-      // 清理资源
-      video.onloadeddata = null;
-      video.src = '';
-
-      resolve(dataURL)
-    }
-  })
-}
-
-const loadVideo = async () => {
-  coverSrc.value = await getVideoCover(props.src, 1)
-}
 
 // 监听video src资源
 watch(() => props.src, () => {
 
 })
-
-onMounted(() => {
-  loadVideo()
-})
 </script>
 
 <template>
   <div ref="container" class="m-video">
-    <img
-      v-if="coverSrc !== undefined"
+    <video
+      v-if="src !== undefined"
       v-bind="attrs"
-      :src="coverSrc"
+      :src="src"
+      :controls="false"
     />
-<!--    <div v-if="isLoading" class="m-video-wrapper">-->
-<!--      <slot name="placeholder">-->
-<!--        <div :class="ns.e('placeholder')" />-->
-<!--      </slot>-->
-<!--    </div>-->
-<!--    <template v-if="preview">-->
-
-<!--    </template>-->
   </div>
 </template>
